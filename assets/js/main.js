c639eed7815a1241a949a224d6e6be2284d7149c
Alex Viapiano <alexviapiano@hotmail.com>
2020-06-03 15:04:06 -0400

    Replaced template with LesRois

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,13 @@
 /*
-	Spectral by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+	Formula by Pixelarity
+	pixelarity.com | hello@pixelarity.com
+	License: pixelarity.com/license
 */
 
 (function($) {
 
 	var	$window = $(window),
-		$body = $('body'),
-		$wrapper = $('#page-wrapper'),
-		$banner = $('#banner'),
-		$header = $('#header');
+		$body = $('body');
 
 	// Breakpoints.
 		breakpoints({
@@ -18,7 +15,8 @@
 			large:    [ '981px',   '1280px' ],
 			medium:   [ '737px',   '980px'  ],
 			small:    [ '481px',   '736px'  ],
-			xsmall:   [ null,      '480px'  ]
+			xsmall:   [ '361px',   '480px'  ],
+			xxsmall:  [ null,      '360px'  ]
 		});
 
 	// Play initial animations on page load.
@@ -28,56 +26,103 @@
 			}, 100);
 		});
 
-	// Mobile?
-		if (browser.mobile)
-			$body.addClass('is-mobile');
-		else {
-
-			breakpoints.on('>medium', function() {
-				$body.removeClass('is-mobile');
-			});
-
-			breakpoints.on('<=medium', function() {
-				$body.addClass('is-mobile');
-			});
-
-		}
-
-	// Scrolly.
-		$('.scrolly')
-			.scrolly({
-				speed: 1500,
-				offset: $header.outerHeight()
-			});
-
 	// Menu.
 		$('#menu')
 			.append('<a href="#menu" class="close"></a>')
 			.appendTo($body)
 			.panel({
+				visibleClass: 'is-menu-visible',
+				target: $body,
 				delay: 500,
 				hideOnClick: true,
 				hideOnSwipe: true,
 				resetScroll: true,
 				resetForms: true,
-				side: 'right',
-				target: $body,
-				visibleClass: 'is-menu-visible'
+				side: 'right'
 			});
 
-	// Header.
-		if ($banner.length > 0
-		&&	$header.hasClass('alt')) {
+	// Banner.
+		var $banner = $('#banner'),
+			$header = $('#header');
 
-			$window.on('resize', function() { $window.trigger('scroll'); });
+		if ($banner.length > 0) {
 
-			$banner.scrollex({
-				bottom:		$header.outerHeight() + 1,
-				terminate:	function() { $header.removeClass('alt'); },
-				enter:		function() { $header.addClass('alt'); },
-				leave:		function() { $header.removeClass('alt'); }
-			});
+			// IE: Height fix.
+				if (browser.name == 'ie') {
+
+					breakpoints.on('>small', function() {
+						$banner.css('height', '100vh');
+					});
+
+					breakpoints.on('<=small', function() {
+						$banner.css('height', '');
+					});
+
+				}
+
+			// More button.
+				$banner.find('.more')
+					.addClass('scrolly');
+
+			// Header.
+				$header
+					.addClass('with-banner')
+					.addClass('alt');
+
+				$banner.scrollex({
+					mode: 'top',
+					top: '-100vh',
+					bottom: 10,
+					enter: function() { $header.addClass('alt'); },
+					leave: function() { $header.removeClass('alt'); }
+				});
 
 		}
+
+	// Spotlights.
+		var $spotlight = $('.spotlight');
+
+		if ($spotlight.length > 0
+		&&	browser.canUse('transition'))
+			$spotlight.each(function() {
+
+				var $this = $(this);
+
+				$this.scrollex({
+					mode: 'middle',
+					top: '-10vh',
+					bottom: '-10vh',
+					initialize: function() { $this.addClass('inactive'); },
+					enter: function() { $this.removeClass('inactive'); }
+				});
+
+			});
+
+	// Features.
+		var $features = $('.features');
+
+		if ($features.length > 0
+		&&	browser.canUse('transition'))
+			$features.each(function() {
+
+				var $this = $(this);
+
+				$this.scrollex({
+					mode: 'middle',
+					top: '-20vh',
+					bottom: '-20vh',
+					initialize: function() { $this.addClass('inactive'); },
+					enter: function() { $this.removeClass('inactive'); }
+				});
+
+			});
+
+	// Scrolly.
+		$('.scrolly').scrolly();
+
+	// Initial scroll.
+		$window.on('load', function() {
+			$window.trigger('scroll');
+		});
 
 })(jQuery);
